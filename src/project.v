5d95556c2eafa55c6390dc5f6a14b5d130b4731b
Alvaro Perez <alvaro.perezn2112@gmail.com>
2026-04-01 16:46:31 -0300

    Actualizacion proyecto 3 ahora de verdad

--- src/project.v
+++ src/project.v
@@ -12,38 +12,3 @@ always @(posedge clk) begin
 end
 
 endmodule
-
-/* 
- * Do not change Module name 
-*/
-module main;
-  // Señales del testbench
-  reg clk;
-  wire [7:0] fib;
-
-  // Instancia del DUT (Device Under Test)
-  fibonacci_8bit uut (
-      .clk(clk),
-      .fib(fib)
-  );
-
-  // Generación de clock (periodo = 10 unidades)
-  initial begin
-      clk = 0;
-      forever #5 clk = ~clk;
-  end
-
-  // Simulación
-  initial begin
-      $display("fib");
-      $monitor("%d", fib);
-
-      // correr simulación un tiempo
-      #140;
-
-      $display("Fin simulacion");
-      $finish;
-  end
-endmodule
-
-
