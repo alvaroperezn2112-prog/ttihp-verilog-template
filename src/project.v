bdaf16b753dc5e2349772e65541cc3f3ca4bef21
Alvaro Perez <alvaro.perezn2112@gmail.com>
2026-04-01 17:25:35 -0300

    AHORA SI

--- src/project.v
+++ src/project.v
@@ -1,25 +1,36 @@
 `default_nettype none
 
 module tt_um_AlvaroPerez2112 (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
+reg [7:0] a;
+reg [7:0] b;
+reg [7:0] fib;
 
-reg [7:0] a = 0, b = 1;
-reg [7:0] fib ;
 assign uo_out = fib;
 
+// No usamos IOs bidireccionales
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
 
-always @(posedge clk) begin
-    a <= b;
-    b <= a + b;
-    fib <= a;
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        a   <= 8'd0;
+        b   <= 8'd1;
+        fib <= 8'd0;
+    end else begin
+        a   <= b;
+        b   <= a + b;
+        fib <= a;
+    end
 end
+
 endmodule
